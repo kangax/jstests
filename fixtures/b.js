0066a1976194810597510e310ab6abd20b74cf60
kangax <kangax@gmail.com>
2010-08-28 10:44:16 -0400

    Add "is currently executed script last script element in a document?" test.

---
+++ fixtures/b.js
@@ -0,0 +1,3 @@
+var scripts = document.getElementsByTagName('script');
+alert('Executing script B (defer). Last script is ' + scripts[scripts.length - 1]
+  .getAttribute('src').replace(/\.js$/, '').replace(/^\/fixtures\//, '').toUpperCase());
---
+++ fixtures/c.js
@@ -0,0 +1,3 @@
+var scripts = document.getElementsByTagName('script');
+alert('Executing script C. Last script is ' + scripts[scripts.length - 1]
+  .getAttribute('src').replace(/\.js$/, '').replace(/^\/fixtures\//, '').toUpperCase());
