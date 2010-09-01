4f9703d790c2bdf749c842ef4758da744e65d39f
kangax <kangax@gmail.com>
2010-09-01 09:49:45 -0400

    Make output in script test more readable.

--- fixtures/a.js
+++ fixtures/a.js
@@ -1,3 +1,3 @@
 var scripts = document.getElementsByTagName('script');
 document.write('Executing script A (async). Last script is ' + scripts[scripts.length - 1]
-  .getAttribute('src').replace(/\.js$/, '').replace(/^\/fixtures\//, '').toUpperCase());
+  .getAttribute('src').replace(/\.js$/, '').replace(/^\.\.\/fixtures\//, '').toUpperCase() + '<br>');
